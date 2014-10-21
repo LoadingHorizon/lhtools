2d970706a8f1a49a6bc65e6f89b043376e362f70
hatlonely <hatlonely@gmail.com>
2014-10-21 16:50:28 +0800

    add template

---
+++ template/leetcode_tpl.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+""" leetcode 模板
+
+Author: hatloney(hatlonely@gmail.com)
+Date: 2014-10-21
+"""
+
+import argparse
+import re
+
+
+cmakelists_tpl = """CMAKE_MINIMUM_REQUIRED(VERSION 2.8)
+PROJECT({project})
+
+SET(CMAKE_VERBOSE_MAKEFILE OFF)
+SET(EXECUTABLE_OUTPUT_PATH ${{PROJECT_BINARY_DIR}}/bin)
+SET(CMAKE_CXX_FLAGS "-g -pipe -W -Wall -fPIC")
+
+INCLUDE_DIRECTORIES(
+    "."
+)
+
+LINK_DIRECTORIES(
+)
+
+IF (class STREQUAL hl)
+    ADD_DEFINITIONS(-DHL=1)
+    SET(SRC_LIST main.cpp {filename}_hl.hpp)
+ENDIF()
+IF (class STREQUAL pj)
+    ADD_DEFINITIONS(-DPJ=1)
+    SET(SRC_LIST main.cpp {filename}_pj.hpp)
+ENDIF()
+
+ADD_EXECUTABLE({filename} ${{SRC_LIST}})
+"""
+
+
+main_tpl = """#include <{filename}.h>
+class TestSolution {{
+public:
+    static void test() {{
+        // TODO your test code
+    }}
+
+private:
+        static Solution _solution;
+}};
+
+Solution TestSolution::_solution;
+
+
+int main() {{
+    TestSolution::test();
+
+    return 0;
+}}
+"""
+
+
+header_tpl = """
+#if defined HL
+    #include <{filename}_hl.hpp>
+#elif defined PJ
+    #include <{filename}_pj.hpp>
+#endif
+"""
+
+
+default_arguments = {
+    'question': 'Two Sum',
+}
+
+
+__params = {}
+
+
+command_example = """
+command example:
+    tplmake.py -t leetcode -q "Two Sum"
+"""
+
+
+def parser():
+    parser = argparse.ArgumentParser(usage=command_example)
+    parser.add_argument('-q', '--question', required=True,
+            help='question name');
+    return parser
+
+
+def params(options):
+    global __params
+    args = parser().parse_args(options)
+    __params['question'] = args.question
+    __params['project'] = ''
+    __params['filename'] = ''
+
+    # "Two Sum" => "tow_sum"
+    pattern = re.compile('([A-Z])')
+    __params['filename'] = pattern.sub(r'_\1', args.question.replace(' ', '')).lower()
+    if __params['filename'].startswith('_'):
+        __params['filename'] = __params['filename'][1:]
+    __params['project'] = 'LEETCODE_' + __params['filename'].upper()
+
+    return __params
+
+
+def tpls():
+    return [cmakelists_tpl, main_tpl, header_tpl]
+
+
+def outs():
+    return ['CMakeLists', 'main.cpp', __params['filename'] + '.h']
