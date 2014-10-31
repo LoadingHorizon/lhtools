b3c909f801dd966518fc9e7d0c49247f9b40ae85
hatlonely <hatlonely@gmail.com>
2014-10-31 12:17:10 -0400

    update leetjava_tpl.py

--- template/leetjava_tpl.py
+++ template/leetjava_tpl.py
@@ -12,14 +12,36 @@ import argparse
 leetcode_java_tpl = """import java.lang.reflect.Method;
 
 public class Test {{
-    @SuppressWarnings("unchecked")
-    static void test(Class c) throws AssertionError, Throwable {{
-        Method method = c.getMethod("method", new Class[]{{}});
-        assert method.invoke(c.newInstance(), {{}}).equals("result");
-        // TODO add your assertion here
+    private static Method method_;
+    private static Class class_;
+
+    private static void test_cases() throws Throwable {{
+        // test_case(new Object[]{{new int[]{{1, 2, 3, 4}}, 8}}, new int[]{{0, 0}});
+        // TODO add your case here
+    }};
+
+    private static void test_case(Object[] args, Object ret) throws Throwable {{
+        // int[] result = (int[]) method_.invoke(class_.newInstance(), args);
+        // int[] expect = (int[]) ret;
+        try {{
+            // TODO add assertions
+            // assert result[0] == expect[0];
+            // assert result[1] == expect[1];
+        }} catch (AssertionError e) {{
+            System.out.println("test case failed: ");
+            // TODO print error infomation
+        }}
     }}
 
-    public static void main(String[] args) {{
+    @SuppressWarnings("unchecked")
+    public static void test(Class c) throws Throwable {{
+        class_ = c;
+        // TODO set method_
+        // method_ = c.getMethod("twoSum", new Class[]{{int[].class, int.class}});
+        test_cases();
+    }}
+
+    public static void main(String[] args) throws Throwable {{
         if (args.length != 1) {{
             System.out.println("Usage: java -ea Test <package>");
             System.out.println("    eg: java -ea Test pj");
@@ -27,15 +49,7 @@ public class Test {{
             return;
         }}
 
-        try {{
-            Class solution = Class.forName(args[0] + ".HelloWorld");
-            test(solution);
-            System.out.println("test case success.");
-        }} catch (AssertionError exception) {{
-            System.out.println("test case failed.");
-        }} catch (Throwable exception) {{
-            System.out.println(args[0] + " not found");
-        }}
+        test(Class.forName(args[0] + ".Solution"));
     }}
 }}
 """
